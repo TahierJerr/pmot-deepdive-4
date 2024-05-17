18dea8de990f5d31ccd85415a94212561968a2c9
Taher Jerjissi <128802571+TahierJerr@users.noreply.github.com>
2024-05-17 09:28:21 +0200

    nice

--- components/footer.tsx
+++ components/footer.tsx
@@ -13,8 +13,8 @@ const Footer = () => {
     
     return (
         <footer className='bg-red-500 w-full mt-12 text-white'>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 mx-auto max-w-screen-lg px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
-                <div className="sm:col-span-1 md:col-span-2">
+            <div className="grid grid-cols-4 grid-rows-4 gap-4 mx-auto max-w-screen-lg px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+                <div className="row-span-2">
                     <div className='grid grid-cols-1 gap-2'>
                         <h1 className='text-2xl font-bold'>PMOT</h1>
                         <a href="https://maps.app.goo.gl/oUk99k2yUrY7uTfg6" className='cursor-pointer'>
@@ -25,7 +25,7 @@ const Footer = () => {
                         <a href='mailto:info@pmot.nl' className='cursor-pointer  underline underline-offset-4'>info@pmot.nl</a>
                     </div>
                 </div>
-                <div className="sm:col-span-1 md:col-span-1">
+                <div className="row-span-2">
                     <div className='grid grid-cols-1 gap-2'>
                         <h1 className='text-2xl font-bold'>{f("categories")}</h1>
                         <Link href='/robotics' className='cursor-pointer  hover:underline'>{c("robotics.title")}</Link>
@@ -36,7 +36,7 @@ const Footer = () => {
                         <Link href='#' className='cursor-pointer hover:underline'>{c("tools")}</Link>
                     </div>
                 </div>
-                <div className="sm:col-span-1 md:col-span-1">
+                <div className="row-span-2">
                     <div className='flex flex-col gap-2'>
                         <h1 className='text-2xl font-bold'>{f("companyinfo")}</h1>
                         <p>KVK: 02057913</p>
@@ -45,7 +45,7 @@ const Footer = () => {
                         <p>Bank BE: IBAN BE68 0682 2823 2034</p>
                     </div>
                 </div>
-                <div className="sm:col-span-1 md:col-span-1">
+                <div className="row-span-2">
                     <div className='grid grid-cols-1 gap-2'>
                         <h1 className='text-2xl font-bold'>{f("legal")}</h1>
                         <a href="#" className='cursor-pointer hover:underline'>{f("terms")}</a>
@@ -54,7 +54,7 @@ const Footer = () => {
                         <Link href="#" className='cursor-pointer hover:underline'>Sitemap</Link>
                     </div>
                 </div>
-                <div className='col-span-1 sm:col-span-2 md:col-span-4 row-start-3 flex items-center flex-col sm:flex-row justify-start sm:justify-between border-t pt-4 border-white'>
+                <div className='col-span-4 row-start-3 flex items-center flex-col sm:flex-row justify-start sm:justify-between border-t pt-4 border-white'>
                     <div className="">
                         <p className='font-bold mb-0.5'>{f("subscribe")}</p>
                         <p>{f("subscribe-subtitle")}</p>
@@ -65,7 +65,7 @@ const Footer = () => {
                     </div>
                     
                 </div>
-                <div className='col-span-1 sm:col-span-2 md:col-span-4 row-start-4 border-t border-white -mt-12 sm:-mt-0  sm:pt-4 pt-0 flex items-center justify-between '>
+                <div className='col-span-4 row-start-4 border-t border-white -mt-12 sm:-mt-0  sm:pt-4 pt-0 flex items-center justify-between '>
                     <div className="flex sm:items-center  justify-start">
                     <p className='text-white text-sm'>© {currentYear} PMOT. {f("allrights")}. {f("allprices")}</p>
                     </div>
